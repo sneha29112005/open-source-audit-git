ad138bff5138225f1e0c57c15ea7b31c6841f3b0
SNEHA MISHRA <sneha.24bce10888@vitbhopal.ac.in>
2026-03-30 00:04:23 +0530

    Create script1.sh

---
+++ project/script/script1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+STUDENT_NAME="Sneha mishra"
+SOFTWARE_CHOICE="Git"
+
+KERNEL=$(uname -r)
+USER_NAME=$(whoami)
+UPTIME=$(uptime -p)
+DATE=$(date)
+
+DISTRO=$(cat /etc/os-release | grep PRETTY_NAME | cut -d= -f2)
+
+echo "================================"
+echo " Open Source Audit — $STUDENT_NAME"
+echo "================================"
+echo "Software: $SOFTWARE_CHOICE"
+echo "Kernel : $KERNEL"
+echo "User : $USER_NAME"
+echo "Uptime : $UPTIME"
+echo "Date : $DATE"
+echo "Distro : $DISTRO"
+echo "License : GPL (Linux Kernel)"
